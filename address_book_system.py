5b05569f849aaebae423afc4170f59b05eeac8aa
SabarinathanYuvaraj <sabarinathanyuvaraj321@gmail.com>
2024-02-14 12:34:51 +0530

    [sabarinathan] Add : python script show how  to Edit a Contact in the  Address Book

--- address_book_system.py
+++ address_book_system.py
@@ -9,7 +9,13 @@ class AddressBook:
         for contact in self.contacts:
             self.display_contact(contact)
         
-
+    def edit_contact(self, first_name, last_name, new_details):
+        for contact in self.contacts:
+            if contact['first_name'] == first_name and contact['last_name'] == last_name:
+                contact.update(new_details)
+                print("Contact updated successfully.")
+                return
+        print("Contact not found.")
 
     def display_contact(self, contact):
         print("First Name:", contact['first_name'])
@@ -54,14 +60,49 @@ class AddressBookMain:
         }
         self.address_book.add_contact(new_contact)
 
+    def edit_contact_from_console(self):
+        first_name = input("Enter First Name of the contact to edit: ")
+        last_name = input("Enter Last Name of the contact to edit: ")
+        new_address = input("Enter New Address: ")
+        new_city = input("Enter New City: ")
+        new_state = input("Enter New State: ")
+        new_zip_code = input("Enter New Zip Code: ")
+        new_phone_number = input("Enter New Phone Number: ")
+        new_email = input("Enter New Email: ")
+
+        new_details = {
+            'address': new_address,
+            'city': new_city,
+            'state': new_state,
+            'zip_code': new_zip_code,
+            'phone_number': new_phone_number,
+            'email': new_email
+        }
+        self.address_book.edit_contact(first_name, last_name, new_details)
+
     def display_all_contacts(self):
         self.address_book.display_all_contacts()
+        
+    def if_you_want_to_edit_contact(self):
+        respone = input("enter yes or no to edit the existing contact")
+        if respone.lower() == "yes" :
+            
+            address_book_main.edit_contact_from_console()
+            
+
 
 
 address_book_main = AddressBookMain()
 
-number_of_contact_insert = int(input(" enter the number of contacts to inserted : "))
+number_of_contact_insert = int(input("Enter the number of contacts to be inserted: "))
 for i in range(number_of_contact_insert):
     address_book_main.add_new_contact_from_console()
 
 address_book_main.display_all_contacts()
+
+
+address_book_main.if_you_want_to_edit_contact()
+
+
+address_book_main.display_all_contacts()
+
